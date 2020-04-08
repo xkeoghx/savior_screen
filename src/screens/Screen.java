2ed311a71404a4a146e2bbc5e593e1a0305b3419
Felipe Rigo Yoshimura <feliperigo_yoshimura@hotmail.com>
2020-04-08 15:18:31 -0300

    Update project version

--- src/screens/Screen.java
+++ src/screens/Screen.java
@@ -1,11 +1,14 @@
 package screens;
 
 import java.awt.Color ;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
 import java.io.IOException;
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.GridLayout;
+import java.awt.Rectangle;
 
 import com.sun.jna.platform.win32.*;
 
@@ -14,12 +17,29 @@ public class Screen { //extends javax.swing.JWindow
 	public static void main(String args[]) throws IOException, InterruptedException, InstantiationException, IllegalAccessException, ClassNotFoundException, UnsupportedLookAndFeelException{
 		
 		 Frame frame = new Frame();
-	        frame.setVisible(true);
+	     frame.setVisible(true);
 	        
-	        WinDef.HWND hwnd = User32.INSTANCE.FindWindow("SunAwtFrame", "Transparent Window");
-	        int wl =  User32.INSTANCE.GetWindowLong(hwnd, WinUser.GWL_EXSTYLE);
-	        wl = wl | 0x80000 | 0x20;
-	        User32.INSTANCE.SetWindowLong(hwnd, WinUser.GWL_EXSTYLE, wl);        
+	     WinDef.HWND hwnd = User32.INSTANCE.FindWindow("SunAwtFrame", "Transparent Window");
+	     int wl =  User32.INSTANCE.GetWindowLong(hwnd, WinUser.GWL_EXSTYLE);
+	     wl = wl | WinUser.WS_EX_LAYERED | WinUser.WS_EX_TRANSPARENT;
+	     User32.INSTANCE.SetWindowLong(hwnd, WinUser.GWL_EXSTYLE, wl);
+	        
+	        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+	        GraphicsDevice[] gs = ge.getScreenDevices();
+	        
+	        for (int j = 0; j < gs.length; j++) { 
+	        	System.out.println(j);
+	            GraphicsDevice gd = gs[j];
+	            GraphicsConfiguration[] gc = gd.getConfigurations();
+	            
+	            Rectangle gcBounds = gc[0].getBounds();
+		        int xoffs = gcBounds.x;
+		        int yoffs = gcBounds.y;
+		        System.out.println(xoffs);
+		        System.out.println(yoffs);
+		        System.out.println(xoffs);
+		        System.out.println(xoffs);
+	        }	            
 	}
 	
 	@SuppressWarnings({ "serial" })
@@ -31,23 +51,29 @@ public class Screen { //extends javax.swing.JWindow
             super("Transparent Window");
             
             setUndecorated(true);
+            
+            setType(Type.UTILITY);
             setBackground(new Color(0, 0, 0, 0));
             setAlwaysOnTop(true);
-            JPanel panel = new JPanel(new GridLayout(4, 4, 3, 3)); //4, 4, 3, 3 60, 60, 3, 3
+            JPanel panel = new JPanel(new GridLayout(8, 7, 1, 1)); //4, 4, 3, 3 60, 60, 3, 3
 
-            for (int i = 0; i <24 ; i++) { //24 2240
+            for (int i = 0; i <56 ; i++) { //24 2240
                 //JLabel l = new JLabel("rigofel", JLabel.CENTER);
                 //l.setSize(30, 30);
-                JLabel l = new JLabel(new ImageIcon(ImageIO.read( Frame.class.getResourceAsStream("/imgs/frame.png" ))), JLabel.CENTER);
+                JLabel l = new JLabel("rigofel", JLabel.CENTER);
                 
+                l.setFont(l.getFont().deriveFont(20.0f));
                 panel.add(l);
             }    
             setContentPane(panel);
             setOpacity((float) 0.2);
+   
             getContentPane().setPreferredSize(Toolkit.getDefaultToolkit().getScreenSize());
-            getRootPane().putClientProperty("apple.awt.draggableWindowBackground", false);
+            
+            //System.out.println(Toolkit.getDefaultToolkit().getScreenSize().setSize(10000, 10000));
+            getContentPane().setBackground(new Color(0.0f, 0.0f, 0.0f, 0.0f));
             pack();
-            setResizable(false);
+            
         }
     }	        
 }
